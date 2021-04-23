7ca0d2d3af7f85a15d46192136f6a2cd86c00c93
ksemenza <ksemenza13@gmail.com>
2021-04-23 19:00:16 -0400

    seeding data

--- data/seeds/02-posts.js
+++ data/seeds/02-posts.js
@@ -10,24 +10,21 @@ exports.seed = function(knex) {
     created_at: "2021-04-23 20:32:37",
     title: "Post Title Updated",
     entry: "Updating Post you must have the user_id in JSON req and the post's id in the url api/post/#",
-    user_id: 1,
-    "reactions": null
+    user_id: 1
 },
         {
     id: 2,
     created_at: "2021-02-23 20:32:37",
     title: "Second Post Title",
     entry: "Entry for seeded data",
-    user_id: 2,
-    reactions: null
+    user_id: 2
 },
         {
     id: 3,
     created_at: "2020-02-23 20:32:37",
     title: "Third Post Title",
     entry: "Entry for seeded data",
-    user_id:3,
-    reactions: null
+    user_id:3
 }
       ]);
     });
